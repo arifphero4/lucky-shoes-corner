2aac7c61a0a204ef8c16168433711678a7924f94
arifphero <ahammadarif232@gmail.com>
2022-03-26 22:30:20 +0600

    update name

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from "./logo.svg";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Lucky One Shop</h3>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lucky Shoes Corner</h1>
     </div>
   );
 }
